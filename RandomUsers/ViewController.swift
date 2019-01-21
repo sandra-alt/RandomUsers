ee512d21e188025dad814e9016fd4562f83220b1
oceangypsy <oleksandra.altukhova@gmail.com>
2019-01-21 13:57:35 +0200

    Initial Commit

---
+++ RandomUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomUsers
+//
+//  Created by  Oleksandra on 1/21/19.
+//  Copyright © 2019 sandra-alt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
